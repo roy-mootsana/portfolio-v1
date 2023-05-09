88e15b1367e1d53ac11c388ecf65cd8dd4cdaee9
bob <roymootsana@gmail.com>
2023-05-09 14:32:06 +0200

    first commit

---
+++ src/Slides/WideScreen/HeroSlide/NameAndJobTitle.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import NameReveal from './NameReveal';
+import TitleReveal from './TitleReveal';
+
+const Container = styled.div`
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+    height:100vh;
+    width:100%;
+    background-color: white;
+    /* border: 1px solid blue; */
+`;
+
+class NameAndJobTitle extends Component {
+  render() {
+    return (
+      <Container>
+        <NameReveal text="Roy Mootsana" fontFam="Valencia" timeDelay={500} />
+        <br />
+        <TitleReveal text="UI Engineer" fontFam="AvenirRoman" timeDelay={1300} />
+    
+   
+      </Container>
+    );
+  }
+}
+
+export default NameAndJobTitle;
